acd41c178ccd230c24273eefefbeef966cfbfc01
P29-ALUNO <marcuusvinicius2008@gmail.com>
2026-09-04 10:04:08 -0300

    Aula1

--- Aula1/Main.java
+++ Aula1/Main.java
@@ -1,5 +1,8 @@
 public class Main{
 public static void main(String[] args) {
-System.out.println("Sistemas de projeto");   
+System.out.println("=========================")
+System.out.println("Sistemas de projeto");  
+System.out.println("=========================")
+System.out.println("Bem vindos ao sistema!");
 }
 }
